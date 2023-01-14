2b15cebc66c0024765de733606e5f886c87d49df
Jwatts82 <wattscodes@gmail.com>
2023-01-14 11:36:24 -0500

    update CORS headers

--- todos-backend/config/application.rb
+++ todos-backend/config/application.rb
@@ -13,7 +13,7 @@ module TodosBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post]
+        resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :options]
       end
     end
 
